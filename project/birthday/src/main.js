5fef3987d5945a58ef7470ee57f0300d75562f91
ff <gzzyyff@gmail.com>
2014-12-31 14:45:25 +0800

    update

--- project/birthday/src/main.js
+++ project/birthday/src/main.js
@@ -2,8 +2,6 @@
 define(function (require) {
     require('./single').init();
     require('./snow').snow();
-    if (location.search.indexOf('a') !== -1) {
-        $('#audio').attr('src', "resource/birthday.mp3");
-    }
+    $('#audio').attr('src', "resource/birthday.mp3");
     console.log('Powered by strwind');
 });
