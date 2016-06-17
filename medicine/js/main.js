03824c522f8eb1e189080ce3ca79a9b2c887d629
fei.yaoff <fei.yaoff@alibaba-inc.com>
2016-06-17 23:09:49 +0800

    同步 行太多时 显示 查看更多按钮  优化

--- medicine/js/main.js
+++ medicine/js/main.js
@@ -6,7 +6,6 @@ $(function () {
     $autoParas.each(function (index, para) {
         var height = $(para).height();
         if (height > SIX_HEIGHT) {
-            $(para).addClass('common-auto-para-ellipsis');
             $('<div class="common-auto-more-action-wrap"><span class="common-auto-more-action">查看更多</span></div>').insertAfter($(para));
         }
     });
@@ -14,7 +13,7 @@ $(function () {
     // 查看更多绑定事件
     var $moreAction = $('.common-auto-more-action');
     $moreAction.on('touch, click', function() {
-        $(this).parent().parent().find('.common-auto-para').removeClass('common-auto-para-ellipsis');
+        $(this).parent().parent().find('.common-auto-para').addClass('common-auto-para-inherit');
         $(this).parent().remove();
     });
 });
